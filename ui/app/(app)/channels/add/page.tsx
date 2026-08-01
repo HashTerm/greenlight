d68e19caa314cd708bb3ac4574024725c2ed39dd
Marko Kostich <markokosticdev@gmail.com>
2026-08-01 15:19:14 +0300

    chore: fix Prettier formatting on Add channel page
    Collapse PageHeader props to one line per Prettier print width.

--- ui/app/(app)/channels/add/page.tsx
+++ ui/app/(app)/channels/add/page.tsx
@@ -5,11 +5,7 @@ import { PageHeader } from '@/components/page-header'
 export default function AddChannelPage() {
   return (
     <div className="space-y-6">
-      <PageHeader
-        description="Register a new platform channel"
-        icon={Plus}
-        title="Add channel"
-      />
+      <PageHeader description="Register a new platform channel" icon={Plus} title="Add channel" />
       <ChannelForm />
     </div>
   )
